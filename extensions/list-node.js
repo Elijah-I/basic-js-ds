4c1b8843a96f9bf7e1c65a8f73fff62b9b40b0fd
Elijah-I <elijah.ivanik@inbox.ru>
2022-08-13 17:58:50 +0300

    never wanna see this again

--- extensions/list-node.js
+++ extensions/list-node.js
@@ -1,10 +1,10 @@
 class ListNode {
-  constructor(x) {
-    this.value = x;
-    this.next = null;
-  }
+	constructor(x) {
+		this.value = x
+		this.next = null
+	}
 }
 
 module.exports = {
-  ListNode
-};
+	ListNode,
+}
--- src/stack.js
+++ src/stack.js
@@ -1,34 +1,23 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js")
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.data = []
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	push(element) {
+		this.data.push(element)
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	pop() {
+		return this.data.pop()
+	}
+
+	peek() {
+		return this.data[this.data.length - 1]
+	}
 }
 
 module.exports = {
-  Stack
-};
+	Stack,
+}
